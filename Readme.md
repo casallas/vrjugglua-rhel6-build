47587a63f6bdc1f55e10a0ffa8030795815e9ca4
Patrick Carlson <carlsonp@keymaker.vrac.iastate.edu>
2013-01-18 14:43:21 -0600

    add qt and cmake

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 This stores (via submodules) the various dependencies for VR-Jugglua.
 
 It also includes a build script that will compile the entire system.  Current testing has been on VRAC RHEL6 only.
+
+Make sure to run:
+
+git submodule update --init --recursive
--- build.sh
+++ build.sh
@@ -7,7 +7,58 @@
 #http://gaarai.com/2009/04/20/git-submodules-adding-using-removing-and-updating/
 
 
+BASEDIR=$(cd $(dirname $0) && pwd)
+SRCDIR=$BASEDIR/submodules
+BUILDDIR=$BASEDIR/build
+
+
+#Create build directory if it doesn't exist
 if [ ! -d "build" ]; then
     echo "Creating build directory."
     mkdir "build"
 fi
+
+
+
+#Build QT
+#https://qt-project.org/wiki/Building_Qt_5_from_Git
+buildQT() {(
+  echo "Starting QT build..."
+  cd $SRCDIR/qt
+  ./configure -opensource -confirm-license -fast -no-phonon -no-phonon-backend -no-webkit -no-multimedia -nomake examples -nomake tests -prefix $BUILDDIR/qt
+  gmake -j 8
+  gmake install
+ )
+}
+
+#Build CMake
+buildCMake() {(
+  echo "Starting CMake build..."
+  mkdir -p $BUILDDIR/cmake
+  cd $SRCDIR/cmake
+  ./bootstrap --prefix=$BUILDDIR/cmake
+  make -j 8
+  make install
+  CMAKEPROGRAM=$BUILDDIR/cmake/bin/cmake
+ )
+}
+
+
+#Build LuaBind
+buildLuaBind() {(
+  echo "Starting LuaBind build..."
+  mkdir -p $BUILDDIR/luabind && cd $BUILDDIR/luabind
+  $CMAKEPROGRAM $SRCDIR/luabind \
+  -DCMAKE_BUILD_TYPE=Release
+  make -j 8
+ )
+}
+
+
+
+
+#Start running through the build process
+
+#buildQT
+buildCMake
+buildLuaBind
